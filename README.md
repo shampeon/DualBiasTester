ea1da4e428165f29eb16900459ec66ae5d319b37
Ian Evans <5759834+shampeon@users.noreply.github.com>
2025-11-15 10:54:22 -0800

    Initial checkin

---
+++ README.md
@@ -0,0 +1,88 @@
+# Dual Bias Tester
+
+A bias tester for tube amplifiers with octal power tubes (e.g. 6L6, 6V6, EL34, 5881). The tester can monitor two tubes at once, allowing you to adjust the bias and while seeing the bias difference between the two tubes to see how well-matched they are.
+
+[!CAUTION]
+Tube amps have high voltage current that can kill you, even when turned off. If you are uncomfortable working with high voltage circuits or taking the risks inherent to working on tube amplifiers, find a qualified tech to work on your amp. Wiring the tester or probes incorrectly is extremely dangerous, and can expose you to lethal current. Always think about what you are doing when working on amplifier circuits, always drain the filter capacitors after powering off the amp, and exercise caution at all times. Use this tester at your own risk.
+
+## Hardware
+
+The tester requires:
+
+* 1 Arduino Nano
+* 1 ADS1115 ADC board
+* 1 128x64 SH1106 or SSD1306/1309 OLED display with I2C
+* 3 momentary push buttons
+* 6 banana sockets
+* 2 octal bias probes that connect to the plate and cathode of the socket under test ([TubeDepot's Bias Scout Kit](https://www.tubedepot.com/products/tubedepot-bias-scout-kit) or equivalent, see section below). Note that the [Hoffman Amps Bias Checker](https://el34world.com/charts/BiasChecker3.htm) will not work, as that probe does not monitor the plate voltage, only the cathode current.
+
+### DIY probes
+
+The TubeDepot Bias Scout kit has everything you need, and is inexpensive at around $40 for two probes. If you want to make your own bias probe, for each probe you will need:
+
+* 1 octal tube base (e.g. [P-SP8-47X from Amplified Parts](https://www.amplifiedparts.com/products/tube-base-8-pin-octal-125-diameter))
+* 1 octal PCB socket (e.g. [P-ST8-810-PCL from Amplified Parts](https://www.amplifiedparts.com/products/socket-8-pin-octal-phenolic-pc-mount-long-lead))
+* 1 1M 1/2 watt or greater 1% resistor
+* 1 100 ohm 1/8 watt or greater 1% resistor
+* 1 1 ohm 2 watt or greater 5% resistor
+* 1 3 conductor wire
+* 3 banana plugs
+
+### Adapting for 9-pin tubes
+
+The bias tester can, in principle, work to bias EL84 and other 9-pin power tubes. It just requires building a probe with the correct inputs for 9-pin tubes. 9-pin power tubes do not use the same pin arrangement as octal power tubes. That means creating a new nonal socket probe, or an adapter for an octal probe. Any nonal probe or adapter must connect to the correct voltage and current input jack and socket pins using the same component values and wiring as the octal probe.
+
+### Wiring the hardware
+
+The Nano, ADC, and OLED display all run on 5v DC, and the ADC and OLED communicate on the I2C bus. With I2C, multiple devices all communicate on the same bus, or wire, so there's only one connection to the Nano. You daisychain I2C devices in parallel on the data and clock wires. See the Adafruit
+
+Nano
+    A4 -> SDA (I2C data)
+    A5 -> SCL (I2C clock)
+    D5 -> button 1 (left)
+    D6 -> button 2 (right)
+    D7 -> button 3 (select)
+    VIN -> VDD/VCC
+    GND -> GND
+
+ADS1115
+    VDD -> VIN (5v DC)
+    GND -> GND
+    SDA -> SDA (I2C data)
+    SCL -> SCL (I2C clock)
+    A0 -> probe A voltage (red wire in Bias Scout)
+    A1 -> probe A current (white wire in Bias Scout)
+    A2 -> probe B voltage (red wire in Bias Scout)
+    A3 -> probe B current (white wire in Bias Scout)
+
+OLED display
+    VCC -> VIN (5v DC)
+    GND -> GND
+    SDA -> SDA (I2C data)
+    SCL -> SCL (I2C clock)
+
+Probe A
+    red plug -> probe A voltage socket
+    white plug -> probe A current socket
+    black plug -> probe A common socket
+
+Probe B
+    red plug -> probe B voltage socket
+    white plug -> probe B current socket
+    black plug -> probe B common socket
+
+## Firmware
+
+The firmware is in `src/main.cpp` and uses [PlatformIO](https://platformio.org/) to build and upload the compiled firmware to the Nano.
+
+### Changing the display
+
+By default, the firmware is configured for an SSD1306/1309 OLED display. If you use an SH1106 OLED display, find the `Display Initialization` section at the beginning of `main.cpp` and uncomment the configuration line for SH1106 and comment out the line for SSD1306/1309.
+
+```cpp
+// SH1106 OLED
+U8X8_SH1106_128X64_NONAME_HW_I2C lcd(A5,A4,U8X8_PIN_NONE);
+//
+// SSD1306/1309 OLED
+//U8X8_SSD1306_128X64_NONAME_HW_I2C lcd(A5, A4, U8X8_PIN_NONE);
+```
---
+++ src/main.cpp
@@ -0,0 +1,428 @@
+// Ver15: 2025-11-13 Updated to latest libraries, added SSD1306 128x64 display, converted to cpp
+
+// Ver14: Implemented option for tubes that don't use screen grids to disable 5.5% grid calculation, added "Debug" tube to check direct measurement.
+// Ver13: Removed unneeded commented-out code sections
+
+// Ver12: Rewrote bias calculation/display section to format values into padded text so flicker-inducing line blanking can be removed
+
+// Next two are needed for the SH1106 compatible OLED library
+#include <Arduino.h>
+#include <U8x8lib.h>
+
+// Of the oroginal code, basically all that's left is the menu. This uses a different display library, an I2C ADC board, and displays two channels at the time time rather than one.
+// Please note that this code is rough and not even properly tidied up, so apologies for the mess.
+// This uses the Adafruit ADS1015 precision ADC board; rather than the internal Arduino ADC. The OLED display also runs on the I2C bus. Pushbuttons go to digital input 5,6,7
+
+/**
+ * This work is licensed under a Creative Commons Attribution 4.0 International License. https://creativecommons.org/licenses/by/4.0/
+ * Please give credit to John Wagner - mrjohnwagner@gmail.com
+ *
+ * ArduinoBiasMeter
+ a bias plug that is read in MILLIVOLTS. At no point should more than 5v be
+ *  read on this meter.
+ *
+ *  Version 1.0 11/28/2017
+ */
+
+// include the display library code:
+#include <Wire.h>
+
+// ADC Library
+#include <Adafruit_ADS1X15.h>
+
+// Display Initialization
+
+// SH1106 OLED
+//  U8X8_SH1106_128X64_NONAME_HW_I2C lcd(A5,A4,U8X8_PIN_NONE);
+//
+// SSD1306/1309 OLED
+U8X8_SSD1306_128X64_NONAME_HW_I2C lcd(A5, A4, U8X8_PIN_NONE);
+
+Adafruit_ADS1115 ads;
+
+// Note Changes these to ints, adjust math in the doBias section to avoid "fuzziness" of floats.
+
+float VoltageA = 0.0;
+float CurrentA = 0.0;
+float ScreenCurrentA = 0.0;
+float VoltageB = 0.0;
+float CurrentB = 0.0;
+float ScreenCurrentB = 0.0;
+float PowerA = 0.0;
+float PowerB = 0.0;
+
+float CurrentAPrevious = 0.0;
+float CurrentBPrevious = 0.0;
+float VoltageAPrevious = 0.0;
+float VoltageBPrevious = 0.0;
+
+bool FirstBiasRun = false; // ensure results are displayed when started up with static inputs
+
+int guiClick = 7; // Selector button
+// Prepping variables for Left/Right Digital buttons
+int guiLClick = 5;
+int guiRClick = 6;
+
+// presently the GUI has two modes
+#define GUI_MENU_SELECT_MODE 0
+#define GUI_BIAS_MODE 1
+// the GUI starts in menu mode
+int guiMode = GUI_MENU_SELECT_MODE;
+boolean movedMenu = false;
+int curMenuItem = 0;
+
+// declare functions
+int processTubeMenu();
+void doBias();
+boolean buttonClicked();
+void displayTubeTypes();
+boolean debouncedButtonPressed(int inputPin);
+
+// tube record
+struct TUBE
+{
+  String name;
+  int maxDissipation;
+  bool screengrid;
+};
+
+// currently supported tubes - Tube name, max wattage, and TRUE/FALSE to subtract 5.5% for screen current
+TUBE tubes[] = {{"6L6GC", 30, true}, {"EL34", 25, true}, {"6V6", 12, true}, {"6L6/G/GB", 19, true}, {"6L6WGB/WGC", 26, true}, {"5881", 23, true}, {"5881WXT", 26, true}, {"6550", 42, true}, {"EL84", 12, true}, {"RAW No Ig2", 100, false}};
+#define arr_len(x) (sizeof(x) / sizeof(*x))
+
+// voltage record for averaging
+struct VOLTAGE_AVG
+{
+  double pinVal;  // average value read from pin
+  int count;      // number of times pin value read
+  int pin;        // which pin
+  double voltage; // calculated voltage
+};
+
+void zero(VOLTAGE_AVG *_this)
+{
+  _this->pinVal = 0;
+  _this->count = 0;
+}
+
+char LeftArrow = 127;
+char RightArrow = 126;
+
+void setup()
+{
+  // set the joystick pin to pullup
+  pinMode(guiClick, INPUT_PULLUP);
+  pinMode(guiRClick, INPUT_PULLUP);
+  pinMode(guiLClick, INPUT_PULLUP);
+
+  // setup the LCD
+
+  // Config for SH1106. Uncomment only one!
+
+  lcd.begin();
+  // lcd.setFont(u8x8_font_5x7_f);
+  lcd.setFont(u8x8_font_artossans8_r);
+  // lcd.setFlipMode(1); //rotate display 180 degrees
+
+  // uncomment and and edit as desired for boot-up screen.
+  lcd.print("  Bias Tester\n Kiel Lydestad\n Ian Evans\n     2025");
+
+  delay(750);
+
+  lcd.clear();
+
+  // Configure ADC Gain
+  ads.setGain(GAIN_SIXTEEN);
+  ads.begin();
+}
+void loop()
+{
+
+  if (guiMode == GUI_MENU_SELECT_MODE)
+  {
+    if (processTubeMenu() == 1)
+    {
+
+      // zero(&plate);
+      // zero(&cathode);
+      guiMode = GUI_BIAS_MODE;
+
+      lcd.clear();
+      lcd.print(tubes[curMenuItem].name);
+      lcd.print(":");
+      lcd.setCursor(0, 1);
+      lcd.print(round(floor(tubes[curMenuItem].maxDissipation * .5)));
+      lcd.print("|");
+      lcd.print(round(floor(tubes[curMenuItem].maxDissipation * .6)));
+      lcd.print("|");
+      lcd.print(round(floor(tubes[curMenuItem].maxDissipation * .7)));
+      lcd.print("|");
+      lcd.print(tubes[curMenuItem].maxDissipation);
+
+      // Screen header writes
+
+      lcd.setCursor(0, 2);
+      lcd.setInverseFont(1);
+      lcd.print(" A:");
+      lcd.setInverseFont(0);
+
+      lcd.setCursor(8, 2);
+      lcd.setInverseFont(1);
+      lcd.print(" B:");
+      lcd.setInverseFont(0);
+
+      lcd.setCursor(0, 7);
+      lcd.setInverseFont(1);
+      lcd.print(" Delta:");
+      lcd.setInverseFont(0);
+
+      FirstBiasRun = true; // Makes sure that initial bias display shows data
+
+      // lcd.print("w");
+    }
+  }
+  else if (guiMode == GUI_BIAS_MODE)
+  {
+    lcd.setCursor(0, 1);
+
+    doBias();
+
+    if (buttonClicked()) // Centre Button clicked
+    {
+      lcd.clear();
+      guiMode = GUI_MENU_SELECT_MODE;
+    }
+  }
+
+  // delay(100);
+}
+
+void doBias()
+{
+  int16_t adc0; // we read from the ADC, we have a sixteen bit integer as a result
+  int16_t adc1;
+  int16_t adc2;
+  int16_t adc3;
+  float aWattPerc = 0;
+  float bWattPerc = 0;
+
+  char DisplayChar[6];
+  float CurrentDiff = 0;
+
+  // Read voltages from all four channels
+  adc0 = ads.readADC_SingleEnded(0); // Channel A Voltage
+  adc1 = ads.readADC_SingleEnded(1); // Channel A Current
+  adc2 = ads.readADC_SingleEnded(2); // Channel B Voltage
+  adc3 = ads.readADC_SingleEnded(3); // Channel B Current
+
+  // Multiply digital output reading by step value per bit
+  VoltageA = (adc0 * 0.078125);
+  CurrentA = (adc1 * 0.0078125);
+  VoltageB = (adc2 * 0.078125);
+  CurrentB = (adc3 * 0.0078125);
+
+  // Subtract 5.5% of reading to approximate screen current
+
+  if (tubes[curMenuItem].screengrid == false)
+  {
+  }
+  else
+  {
+    ScreenCurrentA = (CurrentA * 0.055);
+    ScreenCurrentB = (CurrentB * 0.055);
+    CurrentA = (CurrentA - ScreenCurrentA);
+    CurrentB = (CurrentB - ScreenCurrentB);
+  }
+
+  // Tube A readings
+
+  // Clear line and write data
+
+  // Volts
+  if (VoltageA != VoltageAPrevious || FirstBiasRun)
+  {
+    lcd.setCursor(0, 3);
+    dtostrf(VoltageA, -6, 1, DisplayChar);
+    lcd.print(DisplayChar);
+    lcd.setCursor(6, 3);
+    lcd.print("V");
+  }
+  // mA
+  if (CurrentA != CurrentAPrevious || FirstBiasRun)
+  {
+    lcd.setCursor(0, 4);
+    dtostrf(CurrentA, -5, 1, DisplayChar);
+    lcd.print(DisplayChar);
+    lcd.setCursor(5, 4);
+    lcd.print("mA");
+  }
+
+  // Watts
+  if (VoltageA != VoltageAPrevious || CurrentA != CurrentAPrevious || FirstBiasRun)
+  {
+    lcd.setCursor(0, 5);
+    dtostrf(abs((CurrentA * VoltageA) / 1000), -6, 1, DisplayChar);
+    lcd.print(DisplayChar);
+
+    lcd.setCursor(6, 5);
+    lcd.print("W");
+
+    //% Rating
+    lcd.setCursor(0, 6);
+    aWattPerc = ((CurrentA * VoltageA) / 1000);
+    dtostrf(abs((aWattPerc / tubes[curMenuItem].maxDissipation) * 100), -6, 1, DisplayChar);
+    lcd.print(DisplayChar);
+    lcd.setCursor(6, 6);
+    lcd.print("%");
+  }
+
+  // Tube B readings
+
+  // Volts
+  if (VoltageB != VoltageBPrevious || FirstBiasRun)
+  {
+    lcd.setCursor(8, 3);
+    dtostrf(VoltageB, -6, 1, DisplayChar);
+    lcd.print(DisplayChar);
+    lcd.setCursor(15, 3);
+    lcd.print("V");
+  }
+
+  // mA
+  if (CurrentB != CurrentBPrevious || FirstBiasRun)
+  {
+    lcd.setCursor(8, 4);
+    dtostrf(CurrentB, -5, 1, DisplayChar);
+    lcd.print(DisplayChar);
+
+    lcd.setCursor(14, 4);
+    lcd.print("mA");
+  }
+
+  // Watts
+  if (VoltageB != VoltageBPrevious || CurrentB != CurrentBPrevious || FirstBiasRun)
+  {
+
+    lcd.setCursor(8, 5);
+    dtostrf(abs((CurrentB * VoltageB) / 1000), -6, 1, DisplayChar);
+    lcd.print(DisplayChar);
+    lcd.setCursor(15, 5);
+    lcd.print("W");
+
+    //% Dissipation
+    lcd.setCursor(8, 6);
+    bWattPerc = ((CurrentB * VoltageB) / 1000);
+    dtostrf(abs((bWattPerc / tubes[curMenuItem].maxDissipation) * 100), -6, 1, DisplayChar);
+    lcd.print(DisplayChar);
+    lcd.setCursor(15, 6);
+    lcd.print("%");
+  }
+
+  // Tube A/B difference calculation
+
+  if (CurrentAPrevious != CurrentA || CurrentBPrevious != CurrentB || VoltageAPrevious != VoltageA || VoltageBPrevious != VoltageB || FirstBiasRun)
+  {
+    lcd.setCursor(8, 7);
+    // lcd.print((CurrentA - CurrentB),1);
+
+    CurrentDiff = CurrentA - CurrentB;
+
+    CurrentDiff = abs(CurrentDiff);
+
+    dtostrf(abs(CurrentDiff), -5, 1, DisplayChar);
+    lcd.print(DisplayChar);
+
+    lcd.setCursor(14, 7);
+    lcd.print("mA");
+  }
+
+  // Setting previous data values to evaluate if data has changed
+  CurrentAPrevious = CurrentA;
+  CurrentBPrevious = CurrentB;
+  VoltageAPrevious = VoltageA;
+  VoltageBPrevious = VoltageB;
+  FirstBiasRun = false; // Flag for first-time run to ensure data has written
+}
+
+int processTubeMenu()
+{
+  displayTubeTypes(); // disply the types of tubes
+
+  if (debouncedButtonPressed(guiLClick) == true && movedMenu == false)
+  {
+    lcd.clearLine(1);
+    lcd.clearLine(2);
+    movedMenu = true;
+    curMenuItem--;
+    if (curMenuItem < 0)
+    {
+      curMenuItem = arr_len(tubes) - 1;
+    }
+  }
+  else if (debouncedButtonPressed(guiRClick) == true && movedMenu == false)
+  {
+    lcd.clearLine(1);
+    lcd.clearLine(2);
+    movedMenu = true;
+    curMenuItem++;
+    if ((unsigned)curMenuItem >= arr_len(tubes))
+    {
+      curMenuItem = 0;
+    }
+  }
+
+  else if (debouncedButtonPressed(guiRClick) == false && debouncedButtonPressed(guiLClick) == false)
+  {
+    movedMenu = false;
+  }
+
+  if (buttonClicked()) // joystick clicked
+  {
+    return 1;
+  }
+
+  return 0;
+}
+
+void displayTubeTypes()
+{
+  lcd.setCursor(0, 0);
+  lcd.print("Select Tube");
+  lcd.setCursor(0, 1);
+  lcd.print("<");
+  // lcd.print(" ");
+  lcd.print(tubes[curMenuItem].name);
+  lcd.print(" ");
+  lcd.print(tubes[curMenuItem].maxDissipation);
+  lcd.print("W");
+  lcd.print(">");
+  // lcd.print("   "); //trailing clear removed 7-17-2019
+}
+
+// debounce the button
+
+boolean buttonClicked()
+{
+  if (digitalRead(guiClick) == LOW)
+  {
+    delay(30);
+    if (digitalRead(guiClick) == LOW)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+boolean debouncedButtonPressed(int inputPin)
+{
+  if (digitalRead(inputPin) == LOW)
+  {
+    delay(30);
+    if (digitalRead(inputPin) == LOW)
+    {
+      return true;
+    }
+  }
+  return false;
+}
